40f2154125d104572f9eda233ba09adea7726d4a
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-02-16 15:17:21 -0300

    videos/styles/closeButton

--- src/Components/SectionID/SectionVideosID.jsx
+++ src/Components/SectionID/SectionVideosID.jsx
@@ -12,34 +12,34 @@ export default function SectionID() {
 
   const { id } = useParams();
 
-  const [showVideo, setShowVideo] = useState(false); // Estado local para controlar si se muestra la capa flotante
+  const [showVideo, setShowVideo] = useState(false);
+  const [selectedVideoIndex, setSelectedVideoIndex] = useState(0); // índice del video seleccionado
 
   useEffect(() => {
     dispatch(getSectionID(id));
   }, [id, dispatch]);
 
-  const handleVideoClick = () => {
-    setShowVideo(true); // Muestra la capa flotante cuando se hace clic en el video
+  const handleVideoClick = (index) => {
+    setSelectedVideoIndex(index); // setear el índice del video seleccionado al hacer clic
+    setShowVideo(true);
   };
 
   const handleOverlayClick = () => {
-    setShowVideo(false); // Oculta la capa flotante cuando se hace clic fuera del video
+    setShowVideo(false);
   };
 
   const handleButtonClick = () => {
-    setShowVideo(false); // Oculta la capa flotante cuando se hace clic en el botón X
+    setShowVideo(false);
   };
 
   return (
     <section className=" bg-secondary container ">
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 ">
         {sectionId.data ? (
-          sectionId.data.attributes.videos.data.map((e, id) => (
-            <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
+          sectionId.data.attributes.videos.data.map((e, index) => (
+            <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 " key={index}>
               <div className="bg-image hover-zoom" style={{ width: "370px" }}>
-                <div onClick={handleVideoClick}>
-                  {" "}
-                  {/* Se agrega onClick para mostrar la capa flotante */}
+                <div onClick={() => handleVideoClick(index)}>
                   <video
                     src={`http://localhost:1337${e.attributes.url}`}
                     className="img-top"
@@ -53,18 +53,18 @@ export default function SectionID() {
           <h1>Cargando...</h1>
         )}
 
-        {/* Capa flotante para mostrar el video */}
         {showVideo && (
-          <div className="overlay " onClick={handleOverlayClick}>
+          <div className="overlay modal" onClick={handleOverlayClick}>
             <button className="close-btn" onClick={handleButtonClick}>
               X
-            </button>{" "}
-            {/* Botón X para cerrar el video */}
-            <div className="video-container modal ">
+            </button>
+            <div className="video-container ">
               <ReactPlayer
-                url={`http://localhost:1337${sectionId.data.attributes.videos.data[0].attributes.url}`}
+                url={`http://localhost:1337${sectionId.data.attributes.videos.data[selectedVideoIndex].attributes.url}`}
                 playing
                 controls={true}
+                width="100%"
+                height="100%"
               />
             </div>
           </div>
