97e0909375270302ad83341a024281e344b5a03f
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-02-22 20:27:54 -0300

    card/link

--- src/Components/CardSection/CardSection.jsx
+++ src/Components/CardSection/CardSection.jsx
@@ -14,28 +14,22 @@ export default function CardSection({ title, description, image, id }) {
   return (
     <MDBRow>
       <MDBCol>
-        <MDBCard style={{ width: "300px" }}>
-          <MDBCardImage
-            className="card-img-top "
-            src={`http://localhost:1337${image[0]}`}
-            style={{ width: "100%", height: "240px" }}
-          />
-          <MDBCardBody
-            className="text-center  text-dark"
-            style={{ height: "170px" }}
-          >
-            <MDBCardTitle>{title}</MDBCardTitle>
-            <MDBCardText>{description}</MDBCardText>
-            <Link
-              className="stretched-link position-absolute bottom-0 start-50 translate-middle-x"
-              to={`/home/media/${id}`}
+        <Link to={`/home/media/${id}`}>
+          <MDBCard style={{ width: "300px" }}>
+            <MDBCardImage
+              className="card-img-top "
+              src={`http://localhost:1337${image[0]}`}
+              style={{ width: "100%", height: "240px" }}
+            />
+            <MDBCardBody
+              className="text-center text-dark"
+              style={{ height: "170px" }}
             >
-              <button className="btn btn-secondary mr-2 ">
-                Fotos y Videos
-              </button>
-            </Link>
-          </MDBCardBody>
-        </MDBCard>
+              <MDBCardTitle>{title}</MDBCardTitle>
+              <MDBCardText>{description}</MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </Link>
       </MDBCol>
     </MDBRow>
   );
