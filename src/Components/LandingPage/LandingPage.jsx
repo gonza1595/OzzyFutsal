52e27cf9b86fc6ee2c66148328e04dd7e75f7b6d
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-02-25 12:35:35 -0300

    landingPage/maquetacion

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
 import ozzyfoto from "../assets/ozzy page.jpeg";
+import "./LandingPage.css";
 
 export default function LandingPage() {
   return (
     <div>
-      <div>
-        <NavBar />
-      </div>
-      <div className="row">
-        <section className="container border col">
-          <div>
-            <h1>Bienvenido al lugar donde te ves mejor</h1>
-            <h3>
-              En esta pagina podras disfrutar de tus fotos,
-              <p>
-                vas a poder ver tus jugadas destacadas, goles, polemicas y
-                mas...
+      <NavBar />
+      <main className="pt-5 colorBg mainBg">
+        <div className="container">
+          <section className="row text-white align-items-lg-center justify-content-lg-center">
+            <article className="col-12 col-lg-5">
+              <h2>Bienvenido al lugar donde te ves mejor</h2>
+              <p className="fs-5">
+                {" "}
+                En esta pagina podras disfrutar de tus fotos, vas a poder ver
+                tus jugadas destacadas, goles, polemicas y mas... Todo captado
+                desde el magico celular del Ozzy!! Espero que los disfruten
               </p>
-              <p>Todo captado desde el magico celular del Ozzy!!</p>
-              Espero que los disfruten
-            </h3>
-          </div>
-          <div>
-            <img className="img-fluid" src={ozzyfoto} alt="No image" />
-          </div>
-        </section>
-      </div>
+            </article>
+            <article className="col-12 col-lg-5">
+              <img
+                className="img-fluid"
+                style={{ width: "auto", height: "350px" }}
+                src={ozzyfoto}
+                alt="Bienvenido al lugar donde te ves mejor"
+              />
+            </article>
+          </section>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
