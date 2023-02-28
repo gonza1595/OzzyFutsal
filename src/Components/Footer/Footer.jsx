3379a8fc8fa815b2d3cc80728b906a544289155f
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-02-28 09:02:03 -0300

    landingPage/footer/css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ import {
 
 export default function Footer() {
   return (
-    <MDBFooter className="bg-dark text-center text-white mt-5">
+    <MDBFooter className="bg-dark text-center text-white">
       <MDBContainer className="p-4 pb-0">
         <section className="mb-4">
           <MDBBtn
@@ -55,7 +55,7 @@ export default function Footer() {
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
         © 2023 Copyright:
-        <a className="text-white">OzzyTioFutsal</a>
+        <a className="text-white text-decoration-none"> OzzyTioFutsal</a>
       </div>
     </MDBFooter>
   );
--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -11,22 +11,23 @@ export default function LandingPage() {
   return (
     <div>
       <NavBar />
-      <main className="pt-5 colorBg ">
+      <main className="pt-5 colorBg pb-5">
         <div className="container">
           <section className="row text-white align-items-lg-center justify-content-lg-center">
-            <article className="col-12 col-lg-5">
+            <article className="col-12 col-lg-6 pe-lg-4">
               <h2>Bienvenido al lugar donde te ves mejor</h2>
               <p className="fs-5">
                 {" "}
                 En esta pagina podras disfrutar de tus fotos, vas a poder ver
-                tus jugadas destacadas, goles, polemicas y mas... Todo captado
-                desde el magico celular del Ozzy!! Espero que los disfruten
+                tus jugadas destacadas, tus goles, las polemicas y mucho mas...
+                Todo captado desde el magico celular del Ozzy!! Espero que los
+                disfruten
               </p>
             </article>
-            <article className="col-12 col-lg-5">
+            <article className="col-12 col-lg-6">
               <img
                 className="img-fluid"
-                style={{ width: "auto", height: "350px" }}
+                style={{ width: "100%", height: "350px" }}
                 src={ozzyfoto}
                 alt="Bienvenido al lugar donde te ves mejor"
               />
@@ -35,74 +36,74 @@ export default function LandingPage() {
           <section className="row justify-content-lg-center paddingSection ">
             <article className="col-12 col-lg-6 pt-4 pe-lg-4">
               <a href="/home/category/Primera">
-                <div className="card text-bg-dark border-0 hover-zoom ">
+                <div className="card text-bg-dark border-0 bg-image hover-overlay ripple shadow-1-strong rounded">
                   <img
                     src={primera}
                     style={{ width: "auto", height: "618px" }}
-                    className="card-img"
+                    className="card-img w-100"
                     alt="..."
                   />
-                  <div className="card-img-overlay">
+                  <div className="card-img-overlay mask styleImage">
                     <h5 className="card-title">Primera</h5>
                   </div>
                 </div>
               </a>
             </article>
-            <article className="col-12 col-lg-6 pt-4">
+            <article className="col-12 col-lg-5 pt-4">
               <a href="/home/category/C20">
-                <div className="card text-bg-dark border-0 hover-zoom">
+                <div className="card text-bg-dark border-0 bg-image hover-overlay ripple shadow-1-strong rounded">
                   <img
                     src={c20}
-                    style={{ width: "auto", height: "295px" }}
+                    style={{ width: "auto", height: "350px" }}
                     className="card-img"
                     alt="..."
                   />
-                  <div className="card-img-overlay">
+                  <div className="card-img-overlay mask styleImage">
                     <h5 className="card-title">C 20</h5>
                   </div>
                 </div>
               </a>
               <article className="col-12 col-lg-12 paddingTopArticles">
                 <a href="/home/category/C17">
-                  <div className="card text-bg-dark border-0 hover-zoom">
+                  <div className="card text-bg-dark border-0 bg-image hover-overlay ripple shadow-1-strong rounded">
                     <img
                       src={c17}
                       style={{ width: "auto", height: "295px" }}
                       className="card-img"
                       alt="..."
                     />
-                    <div className="card-img-overlay">
+                    <div className="card-img-overlay mask styleImage">
                       <h5 className="card-title">C 17</h5>
                     </div>
                   </div>
                 </a>
               </article>
             </article>
-            <article className="col-12 col-lg-6 paddingTopArticles pe-lg-4">
+            <article className="col-12 col-lg-6 paddingTopArticles pe-lg-4 pb-5">
               <a href="/home/category/C15">
-                <div className="card text-bg-dark border-0 hover-zoom">
+                <div className="card text-bg-dark border-0 bg-image hover-overlay ripple shadow-1-strong rounded">
                   <img
                     src={ozzyfoto}
                     style={{ width: "auto", height: "295px" }}
                     className="card-img "
                     alt="..."
                   />
-                  <div className="card-img-overlay">
+                  <div className="card-img-overlay mask styleImage">
                     <h5 className="card-title">C 15</h5>
                   </div>
                 </div>
               </a>
             </article>
-            <article className="col-12 col-lg-6 paddingTopArticles">
+            <article className="col-12 col-lg-5 paddingTopArticles">
               <a href="/home/category/C13">
-                <div className="card text-bg-dark border-0 hover-zoom">
+                <div className="card text-bg-dark border-0 bg-image hover-overlay ripple shadow-1-strong rounded">
                   <img
                     src={ozzyfoto}
                     style={{ width: "auto", height: "295px" }}
                     className="card-img"
                     alt="..."
                   />
-                  <div className="card-img-overlay">
+                  <div className="card-img-overlay mask styleImage">
                     <h5 className="card-title">C 13</h5>
                   </div>
                 </div>
@@ -111,7 +112,9 @@ export default function LandingPage() {
           </section>
         </div>
       </main>
-      <Footer />
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
