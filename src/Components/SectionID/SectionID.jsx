408914cdabb970ba0b3d942d60cf9d6856127c06
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-04-11 18:35:50 -0300

    sectionID/fix

--- src/Components/SectionID/SectionID.jsx
+++ src/Components/SectionID/SectionID.jsx
@@ -157,7 +157,6 @@ export default function SectionID() {
                     </div>
                   )}
                 </div>
-                );
               </div>
             </div>
           </div>
